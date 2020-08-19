6a3433343b8368cdeb92f9d50a4c6413172ab184
Martin Godswill Essuman <martinogodswill.essuman@sysdata.it>
2020-08-19 10:23:29 +0200

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Martin Essuman on 19/08/2020.
+//  Copyright © 2020 Martino Godswill Essuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
